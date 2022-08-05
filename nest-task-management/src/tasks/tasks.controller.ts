3a83acc13da10325b8791f7a0f061e400d748d86
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-05 13:33:46 +0530

    getAllTasks()

--- nest-task-management/src/tasks/tasks.controller.ts
+++ nest-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks(){
+        return this.taskService.getAllTasks();
+    }
+}

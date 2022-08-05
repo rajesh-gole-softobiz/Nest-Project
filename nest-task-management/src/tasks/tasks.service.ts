c6f02d40f9702788abaa77267f6c99aee01e4a97
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-05 14:27:17 +0530

    Create a task (Controller)

--- nest-task-management/src/tasks/tasks.service.ts
+++ nest-task-management/src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import {Task} from './task.model';
+import {Task, TaskStatus} from './task.model';
+import {v4 as uuid} from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -8,5 +9,19 @@ export class TasksService {
 
     getAllTasks(): Task[]  {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task{
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(task);
+        return task;
+
+
     }
 }

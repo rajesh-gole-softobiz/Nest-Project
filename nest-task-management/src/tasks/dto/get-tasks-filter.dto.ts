bb145588f0c7b445f4e000ad4a057260adcdc1fe
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-18 16:32:42 +0530

    Validating Task Filtering and Search

--- nest-task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ nest-task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
     search?: string;
 }
